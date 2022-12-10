23b9d3e2715062d1b313608de39fa632512100e7
ayyaz10 <codementee@outlook.com>
2022-12-10 18:49:42 +0500

    first commit

---
+++ guide.md
@@ -0,0 +1,13 @@
+dependencies 
+
+sanity/client: use to connect sanity backend | command: npm install -g @sanity/client
+sanity/image-url: use to connect sanity backend
+react-google-login: for google authentication
+react-icons: react icons
+npm install react-loader-spinner --save
+react-masonry-css: allows to create masonary image grid
+react-router-dom: provide different react routers 
+uuid: unique identifiers
+jwt_decode: it allows us to decode the jwt and get the oobject or ppayload from the jwt 
+
+npm install @sanity/client @sanity/image-url react-icons react-icons react-loader-spinner react-masonry-css react-router-dom uui @react-oauth/google@latest
---
+++ src/componenets/Login.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect } from 'react';
+// import GoogleLogin from '';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
+
+import { Navigate, useNavigate } from 'react-router-dom';
+import {FcGoogle} from 'react-icons/fc'; 
+import logo from '../assets/logo-light.png';
+import bgVideo from '../assets/bg-video.mp4';
+
+
+import { client } from '../client';
+
+const Login = () => {
+  const navigate = useNavigate();
+  function handleCredentialResponse(response) {
+    const userObject = jwt_decode(response.credential)
+    localStorage.setItem('user', JSON.stringify(userObject))
+    // sending data to sanity database
+    const {name, sub, picture} = userObject;
+    const doc = {
+      _id: sub,
+      _type: 'user',
+      userName: name,
+      image: picture
+    }
+    client.createIfNotExists(doc) 
+    .then(()=> {
+      navigate('/', {replace: true})
+    })
+  }
+  // initialize google client with client id
+  // initialize button for login
+
+  useEffect(()=> {
+    // global google it's coming from the script 
+    const IdConfiguration = {
+      client_id: process.env.REACT_APP_GOOGLE_API_TOKEN,
+      callback: handleCredentialResponse,
+    }
+      google.accounts.id.initialize(IdConfiguration);
+
+      google.accounts.id.renderButton(
+        document.getElementById('signInButton'),
+        {theme: "outline", size: "large", shape: "pill", logo_alignment: "center", text: "continue_with", width: 200, locale: "en-au"},
+      )
+  }, [])
+
+  // if anything in this array changes it's going to run the useEffect again
+
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className="relative w-full h-full">
+        <video
+         src={bgVideo} 
+         type="video/mp4"
+         controls={false}
+         muted
+         autoPlay
+         className="w-full h-full object-cover"
+        />
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="p-5">
+          <img src={logo} width="130px" alt="logo" />
+        </div>
+          <div id="signInButton" className="shadow-2xl">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
+
+
+
+{/* <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+            <GoogleLogin 
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              render={(renderProps)=>(
+                <button
+                  type="button"
+                  className='bg-mainColor flex justify-center items-center p-3 rounded-full cursor-pointer outline-none'
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className='mr-4'/>Sign in with Google
+                </button>
+              )}
+            />
+          </GoogleOAuthProvider>; */}
