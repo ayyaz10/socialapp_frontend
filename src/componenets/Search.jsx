b914683740257eb6f5cfb3fb8b9ddea529991f50
ayyaz10 <codementee@outlook.com>
2023-01-24 20:49:49 +0500

    update

--- src/componenets/Search.jsx
+++ src/componenets/Search.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
-const Search = () => {
+import MasonryLayout from './MasonryLayout';
+import { client } from '../client';
+import {feedQuery, searchQuery} from '../utils/data';
+import Spinner from './Spinner';
+import { MdSettingsApplications } from 'react-icons/md';
+
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState(null);
+  const [loading, setloading] = useState(false);
+
+
+  useEffect(() => {
+    if(searchTerm) {
+      setloading(true);
+      const query = searchQuery(searchTerm.toLowerCase())
+      client.fetch(query)
+      .then(data => {
+        setPins(data);
+        setloading(false);
+      })
+    } else {
+      client.fetch(feedQuery)
+      .then(data => {
+        setPins(data);
+        setloading(false);
+      })
+    }
+  }, [searchTerm])
+
   return (
-    <div>Search</div>
+    <div>
+      {loading && <Spinner message="Searching for pins" />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins}/>}
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className='mt-10 text-center text-xl'>No Pins Found!</div>
+      )}
+    </div>
   )
 }
 
